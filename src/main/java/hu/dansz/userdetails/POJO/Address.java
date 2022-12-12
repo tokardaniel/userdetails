f208a11d6b1bbedf97172a99ae19415ac49d7798
Tokár Dániel <tokardaniel5000@gmail.com>
2022-12-12 09:10:00 +0100

    felesleges annotáció törlése

--- src/main/java/hu/dansz/userdetails/POJO/Address.java
+++ src/main/java/hu/dansz/userdetails/POJO/Address.java
@@ -1,12 +1,10 @@
 package hu.dansz.userdetails.POJO;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@NoArgsConstructor
 public class Address implements Serializable {
 
     private static final long serialVersionUID = 1L;
